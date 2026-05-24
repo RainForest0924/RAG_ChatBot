0edadba4c95168e6fae620ae87daac46c240792f
TonyWu <tonywudoli@gmail.com>
2026-05-24 22:06:41 +0800

    [Feat] Document and Metadata

--- testLangChain.py
+++ testLangChain.py
@@ -1,41 +1,41 @@
 from pprint import pprint
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.documents  import Document
 import os
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
-import LearningTool
+from langchain_core.vectorstores import InMemoryVectorStore
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Get the OpenAI API key from environment variables
-#api_key = os.getenv("OPENAI_API_KEY")
+# api_key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(model="gpt-5.4") 
+embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
 
-reference_text = """
-「脈象」是中醫「望、聞、問、切」四診中「切診」的核心，指手指感覺到的脈搏跳動形象。
-透過感知脈搏的頻率、節律、強度、形態與流暢度，中醫師能了解氣血運行狀態、臟腑功能盛衰，進而診斷疾病、分析體質及預測預後。
-以下為脈象的基礎知識簡介：
-一、 把脈的位置：寸口脈中醫把脈主要位於手腕內側的「寸口」部位（橈動脈處），分為「寸、關、尺」三個區域：
-關：位於手腕突起的橈骨莖突處。寸：關部前方（靠近手掌端），對應上半身與頭部。尺：關部後方（靠近手臂端），對應下半身與內臟（如肝、腎）。
-二、 脈象的四大要素醫師在辨識脈象時，會從以下四個維度來體會：
-位（深淺）：脈搏浮在表面（輕按即得）或沉在深處（重按才明顯）。
-數（快慢）：脈搏跳動的次數與節律，正常人一息（呼吸一次）約跳 4~5 次。
-形（形態）：脈管的粗細、軟硬與長短（如感覺像琴弦般緊繃或像水流般圓滑）。
-勢（強弱）：脈搏的力量與流暢度（如充沛有力或微弱無力）。
-"""
+documents = [
+    Document(page_content="LangChain是一個專門用於串接LLM與外部工具而設計的開源專案。",
+             metadata={"source": "官方文件", "author" : "LangChain團隊", "date": "2024-06-01"}),
+    Document(
+        page_content="向量資料庫可以快速進行語義搜尋，提升RAG的精確度。",
+             metadata={"source": "技術部落格", "author" : "技術達人", "date": "2025-05-25"}
+    ),
+    Document(
+        page_content="透過metadata的篩選，更能有效提升資料檢索的精確度。",
+             metadata={"source": "白皮書", "author" : "AI研究社", "date": "2025-07-19"}
+    ),
+]
 
-pulse_prompt = ChatPromptTemplate.from_messages([
-    ('system', '請根據以下參考資料會回答使用者的問題，若資料不足請回答「資料不足，暫時無法回答」：\n\n{ref_text}'),
-    ('human', '{question}')
-])
+vector_store = InMemoryVectorStore.from_documents(documents, embeddings)
 
-prompt_value = pulse_prompt.invoke({"ref_text": reference_text, 
-                                   "question": "請解釋脈象的基本特征。" })
+query = "什麼是LangChain?"
+filter_criteria = {"author": "LangChain團隊"}
+filter_responses = vector_store.similarity_search(query, k=1, filter= lambda doc: doc.metadata.get("author") == filter_criteria["author"])
 
-response = llm.invoke(prompt_value)
-print(response.content)
-
-
+for doc in filter_responses:
+    print(f"Content: {doc.page_content}")
+    print(f"Source: {doc.metadata['source']}")
+    print(f"Author: {doc.metadata['author']}")
+    print(f"Date: {doc.metadata['date']}")
