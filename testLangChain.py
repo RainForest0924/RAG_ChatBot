ffa46d3eddc7f4f101471782b73f5d0431818636
TonyWu <tonywudoli@gmail.com>
2026-06-01 22:19:51 +0800

    [Leraning] Chain Application

--- testLangChain.py
+++ testLangChain.py
@@ -2,10 +2,10 @@ from pprint import pprint
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.documents  import Document
-import os
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,29 +13,34 @@ load_dotenv()
 # Get the OpenAI API key from environment variables
 # api_key = os.getenv("OPENAI_API_KEY")
 
-embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
+llm = ChatOpenAI(model="gpt-5.4")
 
-documents = [
-    Document(page_content="LangChain是一個專門用於串接LLM與外部工具而設計的開源專案。",
-             metadata={"source": "官方文件", "author" : "LangChain團隊", "date": "2024-06-01"}),
-    Document(
-        page_content="向量資料庫可以快速進行語義搜尋，提升RAG的精確度。",
-             metadata={"source": "技術部落格", "author" : "技術達人", "date": "2025-05-25"}
-    ),
-    Document(
-        page_content="透過metadata的篩選，更能有效提升資料檢索的精確度。",
-             metadata={"source": "白皮書", "author" : "AI研究社", "date": "2025-07-19"}
-    ),
-]
+# Generate Keywords
+keywords_prompt = ChatPromptTemplate.from_messages([
+    ("system", "你是一個關鍵詞的生成助手"),
+    ("user", "請產生一個適合用於搜尋的關鍵字，關於：{input}")
+])
+keyword_chain = keywords_prompt | llm
 
-vector_store = InMemoryVectorStore.from_documents(documents, embeddings)
+# Search based on keywords
+search_prompt = ChatPromptTemplate.from_messages([
+    ("system", "妳是一個搜尋與資料統整助手。"),
+    ("user", "請根據以下關鍵字搜尋相關的資訊：{keywords}")
+])
+search_chain = search_prompt | llm
 
-query = "什麼是LangChain?"
-filter_criteria = {"author": "LangChain團隊"}
-filter_responses = vector_store.similarity_search(query, k=1, filter= lambda doc: doc.metadata.get("author") == filter_criteria["author"])
+# Combine
+chain = (
+    {"keywords": keyword_chain, "input": RunnablePassthrough()} |
+    RunnableLambda(lambda x: {
+        "keywords": x["keywords"].content 
+    }) |
+    search_prompt | 
+    llm
+)
 
-for doc in filter_responses:
-    print(f"Content: {doc.page_content}")
-    print(f"Source: {doc.metadata['source']}")
-    print(f"Author: {doc.metadata['author']}")
-    print(f"Date: {doc.metadata['date']}")
+input_text = "RAG在醫療領域如何應用"
+results = chain.invoke(input_text)
+
+print("關鍵字： ",  keyword_chain.invoke({"input": input_text}).content)
+print("搜尋結果： ", results.content)
